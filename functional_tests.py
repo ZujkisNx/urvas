f1fadabd6a9cc5fc96778feebf22353bf66f2460
ZujkisNx <pauliusruzgas@gmail.com>
2013-05-08 20:34:22 +0300

    Succesfully reached AssertionError: New to-do item did not appear in table

--- functional_tests.py
+++ functional_tests.py
@@ -46,7 +46,9 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table") 
+            "New to-do item did not appear in table -- its text was:\n%s" % (
+                table.text,
+            ) 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very 
         # methodical)
