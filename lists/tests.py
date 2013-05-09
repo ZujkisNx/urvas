3a70ecb3a36814d4ec2004dfd059513967e04717
ZujkisNx <pauliusruzgas@gmail.com>
2013-05-09 08:56:32 +0300

    Created model for list Items

--- lists/tests.py
+++ lists/tests.py
@@ -50,4 +50,4 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqaul(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.text, 'Item the second')
